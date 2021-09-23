cd8d7b47f8bb45d0b6a1753ecae9c79e5c3de4e2
wimausberlin <poignon.wim@gmx.de>
2021-09-23 17:57:33 +0200

    add first move

--- move_xarm/test.py
+++ move_xarm/test.py
@@ -1,12 +1,11 @@
 import signal
-import threading
 import time
 from queue import Queue
+from typing import final
 import sys
 
 import numpy as np
 
-import xarm_hand_control as xhc
 from xarm.wrapper import XArmAPI
 
 
@@ -14,11 +13,9 @@ VIDEO_INDEX=4
 ARM_IP = "172.21.72.200"
 
 speed=40
-mvacc=10000
+mvacc=100
 
 arm: XArmAPI = None
-
-COMMAND_QUEUE = Queue()
 
 def sigint_handler(sig, frame):
     print("\nSIGINT Captured, terminating")
@@ -26,13 +23,14 @@ def sigint_handler(sig, frame):
         arm.set_state(4)
         arm.disconnect()
 
+    print(arm)
     sys.exit(0)
 
 signal.signal(signal.SIGINT, sigint_handler)
 
-
 def robot_start() -> XArmAPI:
-    arm = "ALFRED"
+    global arm
+    arm = "dumy"
 
     connected = False
     while not connected:
@@ -53,8 +51,8 @@ def robot_start() -> XArmAPI:
     arm.set_state(state=0)
 
     init_pos=[206.9, 0, 258.7, 180, 0,0]
-    ret = arm.set_position(*init_pos,
-        radius=-1, is_radian=False, wait=True, speed=speed, mvacc=mvacc, relative=False)
+    arm.set_position(*init_pos,
+        radius=-1, is_radian=False, wait=True, speed=speed*2.5, mvacc=mvacc, relative=False)
 
     time.sleep(1)
     print("arm started")
@@ -81,23 +79,21 @@ def worker2(arm: XArmAPI):
     pos=[round(num, 1) for num in pos]
     print(pos)
 
+def move_arm_line(arm: XArmAPI, goal_pos=[0]*6,speed=None, mvacc=None,wait=False):
+    pos=arm.position
+    final_pos=[x + y for x, y in zip(pos, goal_pos)]
+    arm.set_position(*final_pos,speed=speed,mvacc=mvacc,wait=wait)
+
+def test1(arm: XArmAPI):
+    arm.set_servo_angle(1,100,speed=speed,mvacc=mvacc,relative=True, wait=True)
+    arm.set_position(z=200,speed=speed,mvacc=mvacc,relative=True,wait=True)
+    arm.set_tool_position(x=350,speed=speed,mvacc=mvacc,relative=True,wait=True)
+    #arm.set_position(y=100,speed=speed,mvacc=mvacc,relative=True,wait=True)
+    arm.set_servo_angle(servo_id=5,angle=30,speed=speed,mvacc=mvacc, wait=True,is_radian=False)
+
 def main():
     arm = robot_start()
-    worker2(arm)
-    # arm = ""
-    """
-    threading.Thread(target=worker, args=[arm, ], daemon=True).start()
-
-    xhc.process(
-        classification_mode=ClassificationMode.NO_CLASSIFICATION,
-        video_index=VIDEO_INDEX,
-        robot_command_queue=COMMAND_QUEUE,
-        dataset_path=None,
-        model_path=None
-    )
-
-    COMMAND_QUEUE.join()
-    """
+    test1(arm)
 
 if __name__ == "__main__":
     main()
