bf6db64714af83e0eb32bff9cfd8c133e0113b9f
Wim <poignon.wim@gmx.de>
2021-10-01 13:33:22 +0200

    finish scanner_arc

--- move_xarm/scanner.py
+++ move_xarm/scanner.py
@@ -1,3 +1,4 @@
+from os import wait
 import signal
 import time
 from queue import Queue
@@ -85,12 +86,14 @@ def points_line(pos:float=init_pos,height:int=0,length:int=0, start_angle:int=0,
         start_angle-=step
     return points
 
-def points_arc(pos:float=init_pos,height:int=0,start_angle:int=0,end_angle:int=0):
+def points_arc(pos:float=init_pos,height:int=0,length:int=[0,100,200],start_angle:int=0,end_angle:int=0):
     """Get points for the arc"""
     pos[2]+=height #change the height of the scanning
-    points=[pos] #add the first position for the height
-    for i in range(3):
-        
+    points=[] #add the first position for the height
+    for i in range(len(length)):
+        angle=radians(start_angle) if i%2==0 else radians(end_angle)#convert angle in degrees
+        start_pos=[(pos[0]+length[i])*cos(angle),(pos[0]+length[i])*sin(angle)]+pos[2:-1]+[pos[-1]+degrees(angle)] # =[(x+l)*cosA, (x+l)*sinA, z+h, roll, pitch, yaw+A]
+        points.append(start_pos)
     return points
 
 
@@ -100,11 +103,19 @@ def scanner_line(arm: XArmAPI,pos:float=init_pos,height:int=0,length:int=0, star
     for point in arm_pos:
         arm.set_position(*point,speed=speed,mvacc=mvacc,wait=True)
 
-
+def scanner_arc(arm: XArmAPI,pos:float=init_pos,height:int=0,length:int=[0,100,200],start_angle:int=0,end_angle:int=0)->None:
+    """Move arm in arc cercle"""
+    arm_pos=points_arc(pos,height,length,start_angle,end_angle)
+    for i in range(len(arm_pos)):
+        angle=abs(start_angle-end_angle)
+        angle=(-1)*angle if i%2==0 else angle
+        arm.set_position(*arm_pos[i],speed=speed,mvacc=mvacc,wait=True)
+        arm.set_servo_angle(1,angle,speed=speed/2,mvacc=mvacc,relative=True, wait=True)
 
 def main():
     arm = robot_start()
-    scanner_line(arm,init_pos,200,300,100,-100,50)
+    #scanner_line(arm,init_pos,200,300,100,-100,50)
+    #scanner_arc(arm,init_pos,350,[0,300],80,-80)
 
 if __name__ == "__main__":
     main()
